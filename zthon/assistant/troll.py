cabd153fa9673008a5da4f3ceb583ae43cec74bb
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-03-08 22:20:46 +0300

    Update troll.py

--- zthon/assistant/troll.py
+++ zthon/assistant/troll.py
@@ -18,7 +18,7 @@ async def on_plug_in_callback_query_handler(event):
             ids = userid
             if event.query.user_id in ids:
                 reply_pop_up_alert = (
-                    "مطـي الرسـاله مـو الك 🧑🏻‍🦯🦓"
+                    "کەرە ئەمە بۆتۆ نییە 🧑🏻‍🦯🦓"
                 )
             else:
                 encrypted_tcxt = message["text"]
